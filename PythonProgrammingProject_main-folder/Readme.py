c624c0ccda0596f7e6ee5228d17aa3eb4e42daa6
Dj01-glitch <121622552+Dj01-glitch@users.noreply.github.com>
2022-12-30 17:14:18 +0530

    Add files via upload

---
+++ PythonProgrammingProject_main-folder/Readme.py
@@ -0,0 +1,7 @@
+echo "# PythonProgrammingProject_main-folder" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Dj01-glitch/PythonProgrammingProject_main-folder.git
+git push -u origin main
